4bdd814a2afcc726333d426a26fe54650cfa09b3
Jorgetico <166435994+Jorgetico@users.noreply.github.com>
2024-04-08 15:26:19 -0600

    Update mi-archivo.js

--- mi-archivo.js
+++ mi-archivo.js
@@ -4,4 +4,4 @@ console.log('Hola mundo_Jorge_Arguedas_Rodriguez');
 console.log('Hola mundo_Alex_Bolanos_Alfaro');
 console.log('Hola mundo_Carlos_Fallas_Duran');
 console.log('Hola mundo_Dilan_Mena_Chaves);
-console.log('Hola mundo_Andrey_Rodriguez_Garcia');›
+console.log('Hola mundo_Andrey_Rodriguez_Garcia');
